554af60699bb9b5064469ebf437d95c986e4509c
Hydrogen3105 <porinw2000@gmail.com>
2020-10-07 13:38:00 +0700

    scrap stocks data

--- Stock.py
+++ Stock.py
@@ -1,2 +1,14 @@
 class Stock:
-    pass
+    
+    def __init__(self, name, _open, high, low, current, change, per_change, buy, sell, volumn, price):
+        self.name = name
+        self.open = _open
+        self.high = high    
+        self.low = low
+        self.current = current
+        self.change = change
+        self.percent_change = per_change
+        self.buy = buy
+        self.sell = sell 
+        self.volum = volumn
+        self.price = price
